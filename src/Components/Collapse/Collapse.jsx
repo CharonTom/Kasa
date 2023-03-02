0730db4cb80ef53ef2ec4879bcdae25d136df675
Tom Charon <charon.s.tom@gmail.com>
2023-03-02 15:42:47 +0100

    contruction page a propos

--- src/Components/Collapse/Collapse.jsx
+++ src/Components/Collapse/Collapse.jsx
@@ -3,37 +3,28 @@ import { useState } from 'react'
 function Collapse() {
   const [collapseOpen, setCollapseOpen] = useState(false)
 
-  return collapseOpen ? (
+  return (
     <div className="collapse">
       <div className="collapse__header">
         <p className="collapse__header__text">coucou</p>
         <button
-          onClick={() => setCollapseOpen(false)}
+          onClick={() => setCollapseOpen(!collapseOpen)}
           className="collapse__header__button"
         >
           &lt;
         </button>
       </div>
-      <div className="collapse__content">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
-          quas! Est consequatur, enim labore esse nulla expedita laborum eveniet
-          recusandae nostrum nobis eum cum voluptates dolor non maxime magnam!
-          Praesentium.
-        </p>
-      </div>
-    </div>
-  ) : (
-    <div className="collapse">
-      <div className="collapse__header">
-        <p className="collapse__header__text">coucou</p>
-        <button
-          onClick={() => setCollapseOpen(true)}
-          className="collapse__header__button"
-        >
-          &lt;
-        </button>
-      </div>
+
+      {collapseOpen && (
+        <div className="collapse__content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+            quas! Est consequatur, enim labore esse nulla expedita laborum
+            eveniet recusandae nostrum nobis eum cum voluptates dolor non maxime
+            magnam! Praesentium.
+          </p>
+        </div>
+      )}
     </div>
   )
 }
